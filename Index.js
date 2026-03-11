8f0d1639543540ebfec0bb260c5dc107e87ce4be
bbbSDWS <briyidt.bautistaa@gmail.coDASD>
2026-03-11 12:53:01 -0500

    2

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 'use strict';
-const http = require('http');
+http = require('http');
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.end('Hola mundo');
