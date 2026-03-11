7f67988cba242263920aa9f73f35f7fd90c05647
bbbSDWS <briyidt.bautistaa@gmail.coDASD>
2026-03-11 14:02:27 -0500

    ultimo de la clase

--- Index.js
+++ Index.js
@@ -1,7 +1,10 @@
-'use strict';
-const http = require('http');
-const server = http.createServer(function(req, res){
-    res.writeHead(200, { 'content-type': 'text/plain' });
-    res.end('Hola mundo');
+const express = require('express')
+const app = express()
+const port = 3000
+app.get('/prueba', (req, res)=>{
+    res.send('Pipe deja de ser tan canson')
 })
-server.listen(5000);
+
+app.listen(port, () => {
+    console.log('la aplicacion se esta ejecutando por el puerto')
+})
