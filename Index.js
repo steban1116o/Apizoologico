0f78c22315efeb3047ed7f272e81b119dd40847b
bbbSDWS <briyidt.bautistaa@gmail.coDASD>
2026-03-11 13:00:06 -0500

    se agrego json

--- Index.js
+++ Index.js
@@ -1,5 +1,5 @@
 'use strict';
-http = require('http');
+const http = require('http');
 const server = http.createServer(function(req, res){
     res.writeHead(200, { 'content-type': 'text/plain' });
     res.end('Hola mundo');
