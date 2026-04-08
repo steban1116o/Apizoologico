fd1827eee4db3798a537cbca3add493040b64a57
bbbSDWS <briyidt.bautistaa@gmail.coDASD>
2026-04-08 14:03:08 -0500

    inicio sesion

--- Index.js
+++ Index.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const app = express()
 const port = 3000
+const authRoutes = require("./routers/authetication"); 
 app.get('/prueba', (req, res)=>{
     res.send('Pipe deja de ser tan canson')
 })
-
+app.use("/api", authRoutes)
 app.listen(port, () => {
     console.log('la aplicacion se esta ejecutando por el puerto')
 })
